fc4dbcb3e82f607f0cd8eb62cf924b217e4657f6
itamm15 <osinski.mateusz15@gmail.com>
2022-07-09 18:20:49 +0200

    initial commit

---
+++ lib/clockify_requester.ex
@@ -0,0 +1,18 @@
+defmodule ClockifyRequester do
+  @moduledoc """
+  Documentation for `ClockifyRequester`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ClockifyRequester.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/clockify_requester_test.exs
@@ -0,0 +1,8 @@
+defmodule ClockifyRequesterTest do
+  use ExUnit.Case
+  doctest ClockifyRequester
+
+  test "greets the world" do
+    assert ClockifyRequester.hello() == :world
+  end
+end
