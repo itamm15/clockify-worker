29fa9745e8351fca17df9af2c5c126ac116398c9
itamm15 <osinski.mateusz15@gmail.com>
2022-07-10 01:24:49 +0200

    added env's and get_api function

--- lib/integration/clockify.ex
+++ lib/integration/clockify.ex
@@ -10,5 +10,6 @@ defmodule ClockifyRequester.Integration.Clockify do
     HTTPoison.get(url, headers)
   end
 
-  def get_api_key, do: Application.get_env(:clockify_requester, :api_key)
+  @spec get_api_key :: String.t()
+  defp get_api_key, do: Application.get_env(:clockify_requester, :api_key)
 end
