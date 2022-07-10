2c9081905cb46246d6df262fb037ad59fb471e4d
itamm15 <osinski.mateusz15@gmail.com>
2022-07-10 17:54:16 +0200

    created post and get functions

--- lib/integration/clockify.ex
+++ lib/integration/clockify.ex
@@ -1,13 +1,63 @@
 defmodule ClockifyRequester.Integration.Clockify do
   # attributes
+  @reports_api "https://reports.api.clockify.me/v1"
   @api "https://api.clockify.me/api/v1"
 
   @spec get(String.t()) ::
           {:ok, HTTPoison.Response.t()} | {:error, HTTPoison.Error.t()}
   def get(endpoint) do
     url = @api <> endpoint
-    headers = ["X-Api-Key": "#{get_api_key()}", "Content-Type": "applications/json"]
+    headers = ["X-Api-Key": "#{get_api_key()}", "Content-Type": "application/json"]
     HTTPoison.get(url, headers)
+  end
+
+  @spec post(String.t()) ::
+          {:ok, HTTPoison.Response.t()} | {:error, HTTPoison.Error.t()}
+  def post(endpoint) do
+    url = @api <> endpoint
+    headers = ["X-Api-Key": "#{get_api_key()}", "Content-Type": "application/json"]
+    HTTPoison.post(url, headers)
+  end
+
+  @spec post_reports(String.t()) :: {:ok, HTTPoison.Response.t()} | {:error, HTTPoison.Error.t()}
+  def post_reports(endpoint) do
+    url = @reports_api <> endpoint
+    headers = ["X-Api-Key": "#{get_api_key()}", "Content-Type": "application/json"]
+
+    body =
+      %{
+        "dateRangeStart" => "2022-05-01T00:00:00.000",
+        "dateRangeEnd" => "2022-05-31T23:59:59.000",
+        "summaryFilter" => %{
+          "groups" => [
+            "USER"
+          ]
+        }
+      }
+      |> Poison.encode!()
+
+    HTTPoison.post(url, body, headers)
+  end
+
+  @spec post_time_entries(String.t(), String.t(), String.t(), String.t(), String.t(), String.t()) ::
+          {:ok, HTTPoison.Response.t()} | {:error, HTTPoison.Error.t()}
+  def post_time_entries(endpoint, start_date, end_date, project_id, billable, description) do
+    url = @api <> endpoint
+    headers = ["X-Api-Key": "#{get_api_key()}", "Content-Type": "application/json"]
+
+    body =
+      %{
+        "start" => start_date,
+        "end" => end_date,
+        "billable" => billable,
+        "description" => description,
+        "projectId" => project_id,
+        "tagIds" => [],
+        "customFields" => []
+      }
+      |> Poison.encode!()
+
+    HTTPoison.post(url, body, headers)
   end
 
   @spec get_api_key :: String.t()
