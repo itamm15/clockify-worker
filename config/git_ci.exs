a49ec4a05f0491b66f07be2e457496db2edba04a
itamm15 <osinski.mateusz15@gmail.com>
2022-07-12 21:35:16 +0200

    divided into two env's

---
+++ config/git_ci.exs
@@ -0,0 +1,4 @@
+import Config
+
+config :clockify_requester,
+  api_key: System.get_env("API_KEY")
--- config/test.exs
+++ config/test.exs
@@ -3,4 +3,8 @@ import Config
 config :clockify_requester,
   api_key: ""
 
-  System.get_env("API_KEY")
+if System.get_env("CI") do
+  import_config("git_ci.exs")
+else
+  import_config("dev.secret.exs")
+end
